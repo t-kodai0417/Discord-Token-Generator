807ee7479a18ee71b2d07d0c21dcc1a9607c0406
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-02-25 13:35:56 +0900

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,23 +1,4 @@
 import random
 import string
-import os
-from datetime import date
-import datetime
-print("NOTE! 1 token loop = 5 Tokens of different types. ")
-N = input("How many tokens loop?: ")
-count = 0
-current_path = os.path.dirname(os.path.realpath(__file__))
-while(int(count)<int(N)):
-    firstGen = random.choice(string.ascii_letters).upper()+random.choice(string.ascii_letters).upper()+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+ random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    secondGen = "OT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    thirdGen = "OT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    fourthGen = "OT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    fifthGen = "OT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    f= open(current_path+"/"+str("Generated Tokens")+str("")+".txt","a")
-    f.write(firstGen+"\n"+secondGen+"\n"+thirdGen+"\n"+fourthGen+"\n"+fifthGen+"\n")
-    print(firstGen)
-    print(secondGen)
-    print(thirdGen)
-    print(fourthGen)
-    print(fifthGen)
-    count+=1
+def string_py(l):
+    data=(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21)))
