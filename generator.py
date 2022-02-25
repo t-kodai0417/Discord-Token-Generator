d75e5108c29dded26f863d64f1df4d1266867f88
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-02-25 13:44:29 +0900

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import random
 import string
 def string_py(l):
-    data=(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21)))
+    data=(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(l)))
+    print(data)
